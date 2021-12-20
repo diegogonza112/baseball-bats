ff0eb5ab69bb92543e6014ba9ac599416b3fd490
diegogonza112 <diego.gonzalezdelgado@mail.utoronto.ca>
2021-12-20 17:23:24 -0500

    added function to fetch every plate appearance

--- scrape_retrosheet.py
+++ scrape_retrosheet.py
@@ -1,5 +1,6 @@
 import zipfile
 from io import BytesIO
+from typing import List
 from urllib.request import urlopen
 
 team_abr_url = 'https://www.retrosheet.org/TEAMABR.TXT'
@@ -12,8 +13,9 @@ class Scraper:
         self.year = year
         self.event_url = 'https://www.retrosheet.org/events/' + year + 'eve.zip'
         self.team = self.find_abbreviation()
+        self.playerID = self.find_player_id()
 
-    def find_abbreviation(self):
+    def find_abbreviation(self) -> str:
         handle = urlopen(self.event_url)
         file = zipfile.ZipFile(BytesIO(handle.read()))
         for i in file.namelist():
@@ -22,13 +24,23 @@ class Scraper:
                                              self.player):
                     return z.decode("utf-8").split(",")[-2]
 
-    def find_player_id(self):
+    def find_player_id(self) -> str:
         handle = urlopen(self.event_url)
         file = zipfile.ZipFile(BytesIO(handle.read()))
         for z in file.open(self.team + '2004.ROS').readlines():
             if all(j in z.decode("utf-8") for j in self.player):
                 return z.decode("utf-8")[:8]
 
+    def find_every_plate_appearance(self) -> List[str]:
+        plate_a = []
+        handle = urlopen(self.event_url)
+        file = zipfile.ZipFile(BytesIO(handle.read()))
+        for i in file.namelist():
+            for z in file.open(i).readlines():
+                if i.endswith("EVN") or i.endswith("EVA"):
+                    ab = z.decode("utf-8").split(',')
+                    if ab[-2] and self.playerID in ab and ab[0] == 'play':
+                        plate_a.append(ab[-2])
+        return plate_a
 
-sr = Scraper("barry bonds", "2004")
-print(sr.find_player_id())
+
