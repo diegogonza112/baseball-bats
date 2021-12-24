d5fb60a3a282abbb16c80440a2f54f2b0fa36477
diegogonza112 <diego.gonzalezdelgado@mail.utoronto.ca>
2021-12-24 18:25:08 -0500

    HTML parsing for collecting player info, will scale to managers, coaches, and umpires

--- retrosheet_utils/people.py
+++ retrosheet_utils/people.py
@@ -1,7 +1,14 @@
+import textwrap
+
 from retrosheet_utils.scraper import Scraper
 from retrosheet_utils.events import Events
 
+import re
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+CLEANER = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+CATEGORIES = ["Born", "First", "Final", "Bat:", "Throw:", "Height:", "Weight:"]
 
 
 class People(Scraper):
@@ -15,6 +22,23 @@ class People(Scraper):
 
     def find_player_info(self):
         handle = urlopen(self.player_info_url)
+        soup = BeautifulSoup(handle.read(), 'html.parser')
+        clean_text = re.sub(CLEANER, '', str(soup.find_all("table")[0]))
+        final = """"""
+        i = 0
+
+        splt = clean_text.replace(";", "").split()
+        while i <= splt.index("Weight:")+1:
+            if splt[i] == "name":
+                final += (splt[i] + ":" + " ")
+            elif splt[i] == "Born":
+                final += ("\n" + splt[i] + ":" + " ")
+            elif splt[i] in CATEGORIES:
+                final += ("\n" + splt[i] + " ")
+            else:
+                final += (splt[i] + " ")
+            i += 1
+        return final
 
 
-People("Barry Bonds", '2004').find_player_info()
+print(People("Barry Bonds", '2004').find_player_info())
